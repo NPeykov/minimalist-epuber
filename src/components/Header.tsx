4bf68e30b6db70110c3066fe2a76f849ae63ad0a
peyk0v <peykovn@gmail.com>
2024-01-05 18:05:58 -0300

    add Background & Header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <a
+      href="https://github.com/NPeykov"
+      className="pt-8 pl-4 text-4xl hover:text-orange-300 text-white font-headeing"
+      target="_blank"
+    >
+      peyk0v
+    </a>
+  )
+}
+
+export default Header
