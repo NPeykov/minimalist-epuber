48551a1a4461c4ab7b68f512b82c9e2b69aa070d
peyk0v <peykovn@gmail.com>
2024-01-08 22:36:45 -0300

    style: gap-2

--- src/components/WrittingButtons.tsx
+++ src/components/WrittingButtons.tsx
@@ -24,7 +24,7 @@ const WrittingButtons = ({
   }
 
   return (
-    <div className="flex flex-wrap justify-end items-center absolute w-full left-0 -bottom-[14px] gap-4">
+    <div className="flex flex-wrap justify-end items-center absolute w-full left-0 -bottom-[14px] gap-2">
       {/* NOTE: below justify can be remplaced by others type of justify */}
       <ButtonContainer hoverText="Reset to default text" onClick={handleReset}>
         <ResetIcon />
