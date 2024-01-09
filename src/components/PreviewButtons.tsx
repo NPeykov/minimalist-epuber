1c3df6f489a3a5c8f651ba9f51fbca6639ec7aa2
peyk0v <peykovn@gmail.com>
2024-01-08 23:39:21 -0300

    fun template

--- src/components/PreviewButtons.tsx
+++ src/components/PreviewButtons.tsx
@@ -2,10 +2,14 @@ import ButtonContainer from './ButtonContainer'
 import { ReactComponent as CopyHTMLIcon } from '../icons/copyHTML.svg'
 
 const PreviewButtons = () => {
+  const handleCopyHTML = () => {
+    // TODO: do logic
+  }
+
   return (
-    <div className="flex flex-wrap justify-end items-center absolute w-full left-0 -bottom-[14px]">
+    <div className="flex justify-end items-center absolute w-full left-0 -bottom-[14px]">
       {/* NOTE: below justify can be remplaced by others type of justify */}
-      <ButtonContainer hoverText="Copy HTML code" onClick={() => {}}>
+      <ButtonContainer hoverText="Copy HTML code" onClick={handleCopyHTML}>
         <CopyHTMLIcon />
       </ButtonContainer>
     </div>
