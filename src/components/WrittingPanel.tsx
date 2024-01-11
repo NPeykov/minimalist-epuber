9032077ae3a7b7bed9e2f11f0dcf5726f31455c8
peyk0v <peykovn@gmail.com>
2024-01-11 14:02:48 -0300

    fix: blue border

--- src/components/WrittingPanel.tsx
+++ src/components/WrittingPanel.tsx
@@ -8,11 +8,10 @@ const WrittingPanel = ({
   setText: React.Dispatch<React.SetStateAction<string>>
 }) => {
   return (
-    // TODO: fix blue border
     <div className="relative">
       <textarea
         spellCheck="false"
-        className="overflow-y-scroll text-white w-full h-full p-2.5 bg-zinc-700 border border-solid rounded-lg border-orange-400/50 hover:border-orange-400"
+        className="overflow-y-scroll text-white w-full h-full p-2.5 bg-zinc-700 border border-solid rounded-lg border-orange-400/50 hover:border-orange-400 focus:border-orange-400 focus:ring-orange-400"
         style={{ outline: 'none', resize: 'none' }}
         onChange={({ target }) => setText(target.value)}
         value={text}
